bce46c145b5c559e98cf930cff0c6040ff77104b
EgemennSahin <egemen.sahin.23@dartmouth.edu>
2023-02-13 01:00:06 -0500

    Fixed uuidv4 bug'

--- website/src/pages/dashboard.tsx
+++ website/src/pages/dashboard.tsx
@@ -12,86 +12,99 @@ import Link from "next/link";
 import React, { BaseSyntheticEvent, useEffect, useState } from "react";
 import usePremiumStatus from "@/configs/stripe/usePremiumStatus";
 import { createCheckoutSession } from "@/configs/stripe/createCheckoutSession";
+import { uuidv4 } from "@firebase/util";
 
 export default function DashboardPage() {
   const { user } = useAuth();
   const userIsPremium = usePremiumStatus(user);
 
-  const [file, setFile] = useState<Blob | null>();
-  const [uploading, setUploading] = useState(false);
-  const [progress, setProgress] = useState(0);
+  const [processedVideo, setProcessedVideo] = useState<string | null>();
+  const [processing, setProcessing] = useState(false);
 
   useEffect(() => {
-    if (file) {
-      setUploading(false);
+    if (processedVideo) {
+      setProcessing(false);
     }
-  }, [file]);
+  }, [processedVideo]);
 
-  // Function to upload a pre-chosen file onto Firebase Storage when a form is submitted
-  async function handleUpload(event: BaseSyntheticEvent) {
+  useEffect(() => {
+    if (processing) {
+      setProcessedVideo(null);
+    }
+  }, [processing]);
+
+  async function handleFileUpload(event: BaseSyntheticEvent) {
     event.preventDefault();
-    if (uploading || !file) {
-      console.log("Still loading. Wait");
+
+    const file = event.target.files[0];
+    if (!file) {
+      console.log("No file selected");
       return;
-    }
-    setUploading(true);
+    } else {
+      const video = document.createElement("video");
+      video.preload = "metadata";
 
-    const uid = uuidv4();
-    const storageRef = ref(
-      storageUploads,
-      "videos/" + user?.uid + "/uploads/" + uid
-    );
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot: UploadTaskSnapshot) => {
-        setProgress(
-          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
-        );
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
+      video.onloadedmetadata = function () {
+        window.URL.revokeObjectURL(video.src);
+        const duration = video.duration;
+        console.log("Duration: ", duration);
+        if (duration > 60) {
+          console.log("Video too long");
+          return;
         }
-      },
-      (error: StorageError) => {
-        console.log(error.message);
-      },
-      async () => {
-        console.log("Processing video");
-        const response_video_processing = await fetch(
-          "https://private-process-video-px2m4mdiyq-uc.a.run.app",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              video_id: uid,
-              user_id: user?.uid,
-            }),
+
+        console.log("Video is good");
+
+        setProcessing(true);
+
+        const uid = uuidv4();
+        const storageRef = ref(
+          storageUploads,
+          "videos/" + user?.uid + "/uploads" + uid
+        );
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot: UploadTaskSnapshot) => {
+            const progress = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+            console.log("Upload progress: " + progress + "%");
+          },
+          (error: StorageError) => {
+            console.log("Error uploading file: " + error.message);
+          },
+          async () => {
+            // Video processing
+            setProcessedVideo(null);
+
+            console.log("Processing video");
+            const response_video_processing = await fetch(
+              "https://private-process-video-px2m4mdiyq-uc.a.run.app",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  video_id: uid,
+                  user_id: user?.uid,
+                }),
+              }
+            );
+
+            const data = await response_video_processing.json();
+
+            console.log("Cloud function invoked: ", data);
+
+            setProcessedVideo(data.url);
+            setProcessing(false);
           }
         );
-
-        const data = await response_video_processing.json();
-
-        console.log("Cloud function invoked: ", data);
-        setUploading(false);
-        setFile(null);
-        setProgress(0);
-      }
-    );
-  }
-
-  // Function to choose a file from the user's computer
-  function handleChoose(event: BaseSyntheticEvent) {
-    event.preventDefault();
-    setUploading(true);
-    setFile(event.target.files[0]);
+      };
+      video.src = URL.createObjectURL(file);
+    }
   }
 
   // Change user.email to user.displayName
@@ -119,42 +132,13 @@ export default function DashboardPage() {
           <input
             type="file"
             accept="video/*"
-            onChange={handleChoose}
+            onChange={handleFileUpload}
             className="w-10 h-10 opacity-0"
           />
           <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-xl">
             Choose Video
           </div>
         </label>
-
-        {file ? (
-          <form
-            className="h-10 relative rounded-lg py-6 px-20 bg-blue-600 hover:bg-blue-800 transition duration-200"
-            onSubmit={handleUpload}
-          >
-            <button type="submit" disabled={file == null}>
-              <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-xl">
-                Upload
-              </div>
-            </button>
-          </form>
-        ) : (
-          <form className="h-10 relative rounded-lg py-6 px-20 bg-slate-300 transition duration-200">
-            <button type="submit" disabled={file == null}>
-              <div className="absolute inset-0 flex items-center justify-center text-slate-500 font-bold text-xl">
-                Upload
-              </div>
-            </button>
-          </form>
-        )}
-
-        {uploading ? (
-          <div>
-            <ProgressBar progress={progress} />
-          </div>
-        ) : (
-          <></>
-        )}
 
         <Link
           className="h-10 relative rounded-lg py-6 px-20 bg-blue-600 hover:bg-blue-800 transition duration-200"
@@ -178,7 +162,4 @@ export default function DashboardPage() {
       </div>
     </>
   );
-}
-function uuidv4() {
-  throw new Error("Function not implemented.");
 }
