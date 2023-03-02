754792c489579ae644da3d4b765ff8cd4008d5e0
EgemennSahin <egemen.sahin.23@dartmouth.edu>
2023-03-01 20:00:32 -0500

    Update edit-video.tsx

--- website/src/pages/edit-video.tsx
+++ website/src/pages/edit-video.tsx
@@ -31,10 +31,7 @@ export default function EditVideoPage({
 
         <div className="grid grid-cols-2 items-center">
           <div className="flex flex-col items-center justify-center">
-            <VideoPlayer
-              src="https://ff209d4e1f0e870ca470b2ab7570bbc3ff631ee172c21834b006ac5-apidata.googleusercontent.com/download/storage/v1/b/short-zoo-temp-videos/o/created%2F129.170.195.201,35.203.255.114%2F83f63838-f746-406e-9a5d-f0cc19a57a3b?jk=AahUMlv7ToojuM-MpRNhmQhn3ZULYEk5e98JcG36i4qXtrN1wx7u-2AK-xq6A7bbqmb2KYFxnQr8njL5Etl77dzBbrYRNWpS0dTuA0NHeNJtniDeOZsMsae9SmWqP9H58lA-axxxODqaIoQRCvGL89WOwollx9gHVAZltxHh7a1IPWKIheTA9bp5DcrDi69OCTfXicHELiqoamQNi7upjYbKN39xXH4dJWQ7FtxZRJl-6Q6fTfoOviyVrJAgCoE250zVoOjSM1-1Y8jsfJYr0Zg8AmW1tnUTYqzqR10x8x1KMcFAxVbTTZ3lH4MN_VRKjNjOyZEfOnv0ajhGL0f6QJGqym2QcCNhv12h7Qj2Sn40fqFLbbHIOzttN2UXNyHohkvSZjWIWZ09wv-aGfJ14n6-cvTchCdvgWOM7w02A2Q70nsrPSW30TC8ieaCp4znueoibEmUd4w9bGM7N0kuAtluzF8MwsaK7eMukWPybFlqIo1EV_LNDQpsLFCTxh1AE2sQ8-g0Pu-oxn7MAmGMH2KTdGnJx0pPqqhUYNmld-gOd4-9LLtYGbZNXyANzif_UdUUpqHIyuPG0nUU16kQF9j3-e63Mgrc3VDThbx5XoxWa8nxfdFYUrGTJ6RQ3dcN7xwTMRgZ3o-c-pwvJNJ6YPcOUoKQaEIy3rZjFSiwa0xYso7C9HIQDgKTPSe15s9a3rF28xHRcK1zjCdHp4aIsbOAB1b75RBzLwtSDGdWjLgpU1ZyZb3lnKg9t-t4h4mp7-UMqcIsPP51Vb-WM3lZHWi94YnIi1h8_4aa8udDhP2MUzY7qH87jaRwNQWK1zTDzHvWvol-flNgtDndbloLxAbSXmZ-imn6uDyRiujeKSqie8H-DRVE5tKuAHryVf5eFGo4ZDqMPk92gnB7IfGWrl4JWUdZ5juFA0nzgbFCErK2BtT5EJhfVEVz0HRhkmHeO88KoKZ4al_v3xiHB4OOwYe2yFKM8sLWDYZ633oXZ58Ufxh9SRx4KvWcipoeUlAm4EiByNdy1kIdlX71l64t6VfHJ01pSj_ZHwZswN93O7lcDPC6vs67gNHtqfOXdOWlZwLovnyouWEwItNEDAbo0U1mVjrIi-HZ16Arzy76g4vZv6rbH2ekAUixqVjuTYaElZ5vTflvOSvl6eRccrqApcS0-mtJAc4QofS6Y8sF5q1K2EVwLLfNdmfF2DCFxR9picfQQXRskLVB0xNcgnpimH_Ot249ozQz1-uVsAm4qtgAiD3drrs62Fg48rkKF51_rjFC1CjuX6YgYpC-4Smq3sQGdbBXIrpwAw&isca=1"
-              size="medium"
-            />
+            <VideoPlayer src={video_url} size="medium" />
           </div>
 
           <div className="flex h-full flex-col items-center justify-start">
@@ -46,7 +43,9 @@ export default function EditVideoPage({
           <TextButton
             color="primary"
             size="medium"
-            onClick={async () => {}}
+            onClick={async () => {
+              console.log("Upload Link:", upload_transcript);
+            }}
             text={"Submit"}
           />
         </div>
@@ -85,10 +84,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
 
     // Get the video_id and transcribeData from the query
-    // const { video_id, download_transcript, upload_transcript } = context.query;
-    const video_id = "test";
+    const { video_id, download_transcript, upload_transcript } = context.query;
 
-    console.log("Test1");
     // Get the video url
     const parsedCookies = parseCookies(context);
     const video_url_response = await fetch(
@@ -106,26 +103,24 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       }
     );
 
-    console.log("Test2");
+    const video_url = await (await video_url_response.json()).url;
 
-    // const video_url = await (await video_url_response.json()).url;
-
-    // if (!video_id || !video_url || !download_transcript || !upload_transcript) {
-    //   return {
-    //     redirect: {
-    //       destination: "/upload-video",
-    //       permanent: false,
-    //     },
-    //   };
-    // }
+    if (!video_id || !video_url || !download_transcript || !upload_transcript) {
+      return {
+        redirect: {
+          destination: "/upload-video",
+          permanent: false,
+        },
+      };
+    }
 
     return {
       props: {
         uid: token.uid,
-        video_url: "test1",
-        video_id: "test2",
-        download_transcript: "test3",
-        upload_transcript: "test4",
+        video_url,
+        video_id,
+        download_transcript,
+        upload_transcript,
       },
     };
   } catch (error) {
