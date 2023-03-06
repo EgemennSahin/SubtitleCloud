a8e5e962a4bda720c8aaab5c5e57dba36ed96a72
EgemennSahin <egemen.sahin.23@dartmouth.edu>
2023-03-06 14:42:52 -0500

    Update hero.tsx

--- website/src/components/hero.tsx
+++ website/src/components/hero.tsx
@@ -10,12 +10,10 @@ export default function Hero() {
             <div className="animate-blob absolute top-0 -left-4 h-72 w-72 rounded-full bg-teal-100 opacity-70 mix-blend-multiply blur-xl filter"></div>
 
             <div className="animate-blob animation-delay-4000 absolute -bottom-24 right-20 h-72 w-72 rounded-full bg-blue-100 opacity-70 mix-blend-multiply blur-xl filter"></div>
-            <div className="relative">
+            <div className="relative h-64 sm:h-96">
               <Image
-                className="mx-auto h-64 w-fit sm:h-96"
-                width="0"
-                height="0"
-                sizes="100vw"
+                fill
+                style={{ objectFit: "contain" }}
                 alt="hero"
                 src="/images/illustration.png"
               />
