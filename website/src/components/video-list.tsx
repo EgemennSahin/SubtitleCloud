b2da90c22499915015098e38e143f4b56e044793
EgemennSahin <egemen.sahin.23@dartmouth.edu>
2023-03-06 15:09:02 -0500

    Update video-list.tsx

--- website/src/components/video-list.tsx
+++ website/src/components/video-list.tsx
@@ -11,12 +11,7 @@ export default function VideoList({
       <ul className=" grid grid-cols-1 gap-y-12 pb-16 md:grid-cols-2 md:gap-y-16 xl:grid-cols-3">
         {videos.map((video, index) => (
           <li className="flex items-center justify-center" key={index}>
-            <VideoPlayer
-              size="small"
-              title={video.title}
-              uid={video.video_id}
-              src={video.url}
-            />
+            <VideoPlayer size="small" title={video.title} src={video.url} />
           </li>
         ))}
       </ul>
