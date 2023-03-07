90aee0433629e2b5b13994cbd72f182bb9159a82
EgemennSahin <77211233+EgemennSahin@users.noreply.github.com>
2023-03-07 14:27:30 -0500

    Fix build

--- website/src/pages/privacy-policy.tsx
+++ website/src/pages/privacy-policy.tsx
@@ -13,11 +13,8 @@ export default function TermsOfService() {
         We use Your Personal data to provide and improve the Service. By using
         the Service, You agree to the collection and use of information in
         accordance with this Privacy Policy. This Privacy Policy has been
-        created with the help of the{" "}
-        <a
-          href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/"
-          target="_blank"
-        >
+        created with the help of the
+        <a href="https://www.freeprivacypolicy.com/free-privacy-policy-generator/">
           Free Privacy Policy Generator
         </a>
         .
@@ -111,12 +108,8 @@ export default function TermsOfService() {
         </li>
         <li>
           <p>
-            <strong>Website</strong> refers to Shortzo, accessible from{" "}
-            <a
-              href="https://www.shortzoo.com"
-              rel="external nofollow noopener"
-              target="_blank"
-            >
+            <strong>Website</strong> refers to Shortzo, accessible from
+            <a href="https://www.shortzoo.com" rel="external nofollow noopener">
               https://www.shortzoo.com
             </a>
           </p>
@@ -155,7 +148,7 @@ export default function TermsOfService() {
       <h3>Usage Data</h3>
       <p>Usage Data is collected automatically when using the Service.</p>
       <p>
-        Usage Data may include information such as Your Device's Internet
+        Usage Data may include information such as Your Device&quot;s Internet
         Protocol address (e.g. IP address), browser type, browser version, the
         pages of our Service that You visit, the time and date of Your visit,
         the time spent on those pages, unique device identifiers and other
@@ -188,14 +181,14 @@ export default function TermsOfService() {
       <p>
         If You decide to register through or otherwise grant us access to a
         Third-Party Social Media Service, We may collect Personal data that is
-        already associated with Your Third-Party Social Media Service's account,
-        such as Your name, Your email address, Your activities or Your contact
-        list associated with that account.
+        already associated with Your Third-Party Social Media Service&quot;s
+        account, such as Your name, Your email address, Your activities or Your
+        contact list associated with that account.
       </p>
       <p>
         You may also have the option of sharing additional information with the
-        Company through Your Third-Party Social Media Service's account. If You
-        choose to provide such information and Personal Data, during
+        Company through Your Third-Party Social Media Service&quot;s account. If
+        You choose to provide such information and Personal Data, during
         registration or otherwise, You are giving the Company permission to use,
         share, and store it in a manner consistent with this Privacy Policy.
       </p>
@@ -229,14 +222,7 @@ export default function TermsOfService() {
         Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies.
         Persistent Cookies remain on Your personal computer or mobile device
         when You go offline, while Session Cookies are deleted as soon as You
-        close Your web browser. Learn more about cookies on the{" "}
-        <a
-          href="https://www.freeprivacypolicy.com/blog/sample-privacy-policy-template/#Use_Of_Cookies_And_Tracking"
-          target="_blank"
-        >
-          Free Privacy Policy website
-        </a>{" "}
-        article.
+        close Your web browser. Learn more about cookies on the article.
       </p>
       <p>
         We use both Session and Persistent Cookies for the purposes set out
@@ -318,10 +304,11 @@ export default function TermsOfService() {
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application's push notifications regarding updates
-            or informative communications related to the functionalities,
-            products or contracted services, including the security updates,
-            when necessary or reasonable for their implementation.
+            such as a mobile application&quot;s push notifications regarding
+            updates or informative communications related to the
+            functionalities, products or contracted services, including the
+            security updates, when necessary or reasonable for their
+            implementation.
           </p>
         </li>
         <li>
@@ -419,12 +406,13 @@ export default function TermsOfService() {
       </p>
       <h2>Transfer of Your Personal Data</h2>
       <p>
-        Your information, including Personal Data, is processed at the Company's
-        operating offices and in any other places where the parties involved in
-        the processing are located. It means that this information may be
-        transferred to — and maintained on — computers located outside of Your
-        state, province, country or other governmental jurisdiction where the
-        data protection laws may differ than those from Your jurisdiction.
+        Your information, including Personal Data, is processed at the
+        Company&quot;s operating offices and in any other places where the
+        parties involved in the processing are located. It means that this
+        information may be transferred to — and maintained on — computers
+        located outside of Your state, province, country or other governmental
+        jurisdiction where the data protection laws may differ than those from
+        Your jurisdiction.
       </p>
       <p>
         Your consent to this Privacy Policy followed by Your submission of such
@@ -496,7 +484,7 @@ export default function TermsOfService() {
         means to protect Your Personal Data, We cannot guarantee its absolute
         security.
       </p>
-      <h1>Children's Privacy</h1>
+      <h1>Children&quot;s Privacy</h1>
       <p>
         Our Service does not address anyone under the age of 13. We do not
         knowingly collect personally identifiable information from anyone under
@@ -509,15 +497,15 @@ export default function TermsOfService() {
       <p>
         If We need to rely on consent as a legal basis for processing Your
         information and Your country requires consent from a parent, We may
-        require Your parent's consent before We collect and use that
+        require Your parent&quot;s consent before We collect and use that
         information.
       </p>
       <h1>Links to Other Websites</h1>
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party's site. We strongly advise You to review the Privacy Policy
-        of every site You visit.
+        third party&quot;s site. We strongly advise You to review the Privacy
+        Policy of every site You visit.
       </p>
       <p>
         We have no control over and assume no responsibility for the content,
--- website/src/pages/tos.tsx
+++ website/src/pages/tos.tsx
@@ -30,10 +30,7 @@ export default function TermsOfService() {
       <p>
         These terms require the use of arbitration Section 10.2 on an individual
         basis to resolve disputes and also limit the remedies available to you
-        in the event of a dispute. These Terms of Use were created with the help
-        of the
-        <a href="https://www.termsofusegenerator.net">Terms Of Use Generator</a>
-        .
+        in the event of a dispute.
       </p>
 
       <h2>Access to the Site</h2>
@@ -87,8 +84,8 @@ export default function TermsOfService() {
       <h2>User Content</h2>
 
       <p>
-        <strong>User Content.</strong> "User Content" means any and all
-        information and content that a user submits to the Site. You are
+        <strong>User Content.</strong> &quot;User Content&quot; means any and
+        all information and content that a user submits to the Site. You are
         exclusively responsible for your User Content. You bear all risks
         associated with use of your User Content.  You hereby certify that your
         User Content does not violate our Acceptable Use Policy.  You may not
@@ -114,9 +111,9 @@ export default function TermsOfService() {
 
       <p>
         <strong>Acceptable Use Policy.</strong> The following terms constitute
-        our "Acceptable Use Policy": You agree not to use the Site to collect,
-        upload, transmit, display, or distribute any User Content (i) that
-        violates any third-party right or any intellectual property or
+        our &quot;Acceptable Use Policy&quot;: You agree not to use the Site to
+        collect, upload, transmit, display, or distribute any User Content (i)
+        that violates any third-party right or any intellectual property or
         proprietary right; (ii) that is unlawful, harassing, abusive, tortious,
         threatening, harmful, invasive of another’s privacy, vulgar, defamatory,
         false, intentionally misleading, trade libelous, pornographic, obscene,
@@ -212,11 +209,11 @@ export default function TermsOfService() {
         action of every kind and nature, that has arisen or arises directly or
         indirectly out of, or that relates directly or indirectly to, the Site.
         If you are a California resident, you hereby waive California civil code
-        section 1542 in connection with the foregoing, which states: "a general
-        release does not extend to claims which the creditor does not know or
-        suspect to exist in his or her favor at the time of executing the
-        release, which if known by him or her must have materially affected his
-        or her settlement with the debtor."
+        section 1542 in connection with the foregoing, which states: &quot;a
+        general release does not extend to claims which the creditor does not
+        know or suspect to exist in his or her favor at the time of executing
+        the release, which if known by him or her must have materially affected
+        his or her settlement with the debtor.&quot;
       </p>
 
       <p>
@@ -231,18 +228,18 @@ export default function TermsOfService() {
       <h2>Disclaimers</h2>
 
       <p>
-        The site is provided on an "as-is" and "as available" basis, and company
-        and our suppliers expressly disclaim any and all warranties and
-        conditions of any kind, whether express, implied, or statutory,
-        including all warranties or conditions of merchantability, fitness for a
-        particular purpose, title, quiet enjoyment, accuracy, or
-        non-infringement.  We and our suppliers make not guarantee that the site
-        will meet your requirements, will be available on an uninterrupted,
-        timely, secure, or error-free basis, or will be accurate, reliable, free
-        of viruses or other harmful code, complete, legal, or safe.  If
-        applicable law requires any warranties with respect to the site, all
-        such warranties are limited in duration to ninety (90) days from the
-        date of first use.
+        The site is provided on an &quot;as-is&quot; and &quot;as
+        available&quot; basis, and company and our suppliers expressly disclaim
+        any and all warranties and conditions of any kind, whether express,
+        implied, or statutory, including all warranties or conditions of
+        merchantability, fitness for a particular purpose, title, quiet
+        enjoyment, accuracy, or non-infringement.  We and our suppliers make not
+        guarantee that the site will meet your requirements, will be available
+        on an uninterrupted, timely, secure, or error-free basis, or will be
+        accurate, reliable, free of viruses or other harmful code, complete,
+        legal, or safe.  If applicable law requires any warranties with respect
+        to the site, all such warranties are limited in duration to ninety (90)
+        days from the date of first use.
       </p>
 
       <p>
@@ -384,7 +381,7 @@ export default function TermsOfService() {
       </p>
 
       <p>
-        <strong>Notice Requirement and Informal Dispute Resolution.</strong>{" "}
+        <strong>Notice Requirement and Informal Dispute Resolution.</strong>
         Before either party may seek arbitration, the party must first send to
         the other party a written Notice of Dispute describing the nature and
         basis of the claim or dispute, and the requested relief.  A Notice to
@@ -429,7 +426,7 @@ export default function TermsOfService() {
       </p>
 
       <p>
-        <strong>Additional Rules for Non-Appearance Based Arbitration.</strong>{" "}
+        <strong>Additional Rules for Non-Appearance Based Arbitration.</strong>
         If non-appearance based arbitration is elected, the arbitration shall be
         conducted by telephone, online and/or based solely on written
         submissions; the specific manner shall be chosen by the party initiating
@@ -575,19 +572,19 @@ export default function TermsOfService() {
         to exercise or enforce any right or provision of these Terms shall not
         operate as a waiver of such right or provision. The section titles in
         these Terms are for convenience only and have no legal or contractual
-        effect. The word "including" means "including without limitation". If
-        any provision of these Terms is held to be invalid or unenforceable, the
-        other provisions of these Terms will be unimpaired and the invalid or
-        unenforceable provision will be deemed modified so that it is valid and
-        enforceable to the maximum extent permitted by law.  Your relationship
-        to Company is that of an independent contractor, and neither party is an
-        agent or partner of the other.  These Terms, and your rights and
-        obligations herein, may not be assigned, subcontracted, delegated, or
-        otherwise transferred by you without Company’s prior written consent,
-        and any attempted assignment, subcontract, delegation, or transfer in
-        violation of the foregoing will be null and void.  Company may freely
-        assign these Terms.  The terms and conditions set forth in these Terms
-        shall be binding upon assignees.
+        effect. The word &quot;including&quot; means &quot;including without
+        limitation&quot;. If any provision of these Terms is held to be invalid
+        or unenforceable, the other provisions of these Terms will be unimpaired
+        and the invalid or unenforceable provision will be deemed modified so
+        that it is valid and enforceable to the maximum extent permitted by
+        law.  Your relationship to Company is that of an independent contractor,
+        and neither party is an agent or partner of the other.  These Terms, and
+        your rights and obligations herein, may not be assigned, subcontracted,
+        delegated, or otherwise transferred by you without Company’s prior
+        written consent, and any attempted assignment, subcontract, delegation,
+        or transfer in violation of the foregoing will be null and void. 
+        Company may freely assign these Terms.  The terms and conditions set
+        forth in these Terms shall be binding upon assignees.
       </p>
 
       <p>
